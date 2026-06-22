790073f55baca4af8f8ad712405ebff6026f765d
Vini <222865488+0xcKy@users.noreply.github.com>
2026-06-22 16:33:55 -0300

    Update rss.py
    Minor changes

--- rss.py
+++ rss.py
@@ -5,19 +5,19 @@ rss_url = ['https://feedexample.com/feed.xml'] #list with feeds
 filename = "intel_report.html"
 
 def get_rss():
-    result_header = "<html lang=\"pt-BR\"><head><meta charset=\"UTF-8\"><title>Intelligence Report</title></head>\n"
+    result_header =f"<html lang=\"pt-BR\"><head><meta charset=\"UTF-8\"><title>Intel Report - %s</title></head>\n" %(date.today())
     result_string = ""
     result_end = "\n</html>"
     for i in rss_url:
         rss_feed = feedparser.parse(i)
         if rss_feed.status == 200:
-            result_string+=(f"<h2>Feed Title: %s</h2>" %(rss_feed.feed.title))
             try:
-                result_string+=(f"<strong>Published: %s</strong>" %(rss_feed.feed.published))
+                result_string+=(f"<h2>Feed Title: %s</h2>" %(rss_feed.feed.title))
+                result_string+=(f"<strong>Collected @ %s</strong>" %(datetime.now()))
             except:
                 pass
             for entry in rss_feed.entries:
-                result_string+=("<p><strong>[+] %s - <a href=\"%s\" rel=\"noopener noreferrer nofollow\" target=\"_blank\">Link de refer&ecirc;ncia</a> [+]</strong><br>%s</p>" %(entry.title, entry.link, entry.description))
+                result_string+=("<p><strong>[+] %s - <a href=\"%s\" rel=\"noopener noreferrer nofollow\" target=\"_blank\">Reference link</a> - %s</strong><br>%s</p>" %(entry.title, entry.link, entry.published, entry.description))
         else:
             print("Failed to get RSS feed. Status code:", rss_feed.status)
     write_file(result_header+result_string+result_end)
