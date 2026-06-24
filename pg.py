66558fd753468ab72c78aac689f4bf28c5481fe7
Vini <222865488+0xcKy@users.noreply.github.com>
2026-06-24 16:02:55 -0300

    Update pg.py
    Now using .env variables via dotenv

--- pg.py
+++ pg.py
@@ -1,46 +1,30 @@
 #file used to connect to postgresql, called on rss.py
-#still needs configuration file containing sensetive data as hostname, database, username, password and port. Will be added later.
 
 import psycopg
+import os
+from dotenv import load_dotenv
 
-#use config file for the sensetive data
-hostname = 'localhost'
-database = 'db_name'
-username = 'postgres'
-pwd = 'password'
-port_id = 'port'
+load_dotenv()
+
 cur = None
 conn = None
-
-def pg_db_write(i):
-    pg_connect(i)
 
 def pg_connect(i):
     try:
         with psycopg.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id) as conn:
+            host = os.getenv('HOSTNAME'),
+            dbname = os.getenv('DATABASE_NAME'),
+            user = os.getenv('USERNAME'),
+            password = os.getenv('PASSWORD_TEXT'),
+            port = os.getenv('PORT_ID')) as conn:
 
             with conn.cursor() as cur:
-
-                create_script = ''' CREATE TABLE IF NOT EXISTS news (
-                                        id BIGSERIAL PRIMARY KEY,
-                                        source VARCHAR(255),
-                                        title TEXT NOT NULL,
-                                        url TEXT NOT NULL,
-                                        published TIMESTAMP,
-                                        content TEXT,
-                                        collected_at TIMESTAMP NOT NULL)'''
-                cur.execute(create_script)
-
-                insert_script = 'INSERT INTO news (source, title, url, published, content, collected_at) VALUES (%s, %s, %s, %s, %s, %s)'
+                table = os.getenv('TABLE_NAME')
+                insert_script = f'INSERT INTO {table} (source, title, url, published, content, collected_at) VALUES (%s, %s, %s, %s, %s, %s)'
                 insert_value = ((i["source"]), (i["title"]), (i["url"]), (i["published"]), (i["content"]), (i["collected_at"]))
                 cur.execute(insert_script, insert_value)
 
-                cur.execute('SELECT * FROM news')
+                cur.execute(f'SELECT * FROM {table}')
                 print(cur.fetchall())
 
     except Exception as error:
@@ -48,6 +32,3 @@ def pg_connect(i):
     finally:
         if conn is not None:
             conn.close()
-
-if __name__ == '__main__':
-    pg_connect()
